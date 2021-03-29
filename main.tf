d7eb436c595a0e42ee02fa8280f5d2b0cb233329
ramesh-DevOps <73949769+ramesh-DevOps@users.noreply.github.com>
2021-03-29 10:55:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,11 +14,11 @@ resource "aws_instance" "windows" {
   subnet_id              = local.subnets
   connection{
   type = "winrm"
-  port = 5986      
+  port = 5985     
   host = "ec2-54-213-60-118.us-west-2.compute.amazonaws.com"
   user = "Administrator"
   password = "${var.admin_password}"
-  https = true
+  https = false
   insecure = true  
   }
   tags = {
